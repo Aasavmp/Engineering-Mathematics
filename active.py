d4ec68c06422d6ececd1ef4045cc286d9aba6a6f
Aasav <aasavmo@gmail.com>
2021-05-23 18:21:31 +0100

    Create files

--- active.py
+++ active.py
@@ -1,10 +1,14 @@
 from scipy.optimize import fsolve
+
+import Stats_functions
+from vector_functions import *
 import vector_functions as vt
-import Stats_functions as st
-import partial_differentiation_functions as pde
+from Stats_functions import *
+import partial_differentiation_functions as pdf
 import math
 import sympy as sym
 import numpy as np
+import matplotlib.pyplot as plt
 from numpy import linalg as la
 from scipy import integrate
 from sympy import cos, sin, pi
@@ -12,51 +16,23 @@ from sympy.vector import ParametricRegion, vector_integrate
 from sympy import *
 init_printing()
 
-x, y, z, i, j, k, t, u, v, a, b, c, r, theta, phi = sym.symbols('x y z i j k t u v a b c r theta phi')
+x, y, z, i, j, k, t, u, v, a, b, c, r, s, w, theta, phi, omega = sym.symbols('x y z i j k t u v a b c r s w theta phi omega')
+
+# pprint(vt.flux_integral([(x**3)*y, -(x**2)*z**2, -(x**2)*y*z], [u*cos(v), u*sin(v), sqrt(u**2-1)], [1, 5], [0, 2*pi]))
+
+# pprint(pdf.inverse_laplace_transform((-2*s-2)/(s**2+4*s+8)))
+
+#pprint(pdf.initial_value_problems_laplace([1, 8, 25], cos(3*t), 2, 1))
+
+#pprint(vt.gauss([(x*y**2), (y*cos(z) + y*z**2), (x**2*z-sin(z))], [0, 1], [0, 2*pi], [0, pi], [r*cos(u)*sin(v), r*sin(u)*sin(v), r*cos(v)]))
+
+#pprint(pdf.continuous_fourier_series(t, 2))
+
+function = lambda x: (2*(-1)**(x+1))/((x*pi)) if x != 0 else 0
+
+#function = lambda x: sin(x) if sin(x) != 0 else 0
+
+#pprint(Stats_functions.t_statistic(5.006, 5.105, sqrt(0.124), 50))
 
 
-function_1 = [a*r*cos(phi)*sin(theta),b*r*sin(phi)*sin(theta),c*r*cos(theta)]
-function_2 = [x-z,z**2,z]
-function_3 = (phi)
-order = [x,y,z]
-x_bounds = [0,pi]
-y_bounds = [0,a]
-z_bounds = [0,3]
-#p = [1,-3,2]
-
-
-
-wrt1 = vt.vector_partial_derivative(function_1,theta)
-wrt2 = vt.vector_partial_derivative(function_1,phi)
-#pprint(wrt1)
-#pprint(wrt2)
-
-cross = vt.cross_product(wrt1,wrt2)
-
-#pprint(cross)
-
-modulus = vt.magnitudealternate(cross)
-
-#mod_squared = modulus**2
-
-#pprint(sim)
-
-pprint(vt.triple_matrix_det(([sin(u),2,0],[7,1,2],[0,5,1])))
-
-X = a*r*sin(theta)*cos(phi)
-Y = b*r*sin(theta)*sin(phi)
-Z = c*r*cos(theta)
-term = [X, Y, Z]
-variables = [r, theta, phi]
-jacobian = []
-for i in range(3):
-   new_row = []
-   differential = term[i]
-   for j in range(3):
-       value = sym.diff(differential, variables[j])
-       new_row.append(value)
-   jacobian.append(new_row)
-array = np.array(jacobian)
-det = vt.triple_matrix_det(array)
-det1 = sym.simplify(det)
-pprint(det1)
+pprint(vt.flux_integral([x*y, x*y, z], [u*cos(v), u*sin(v), u], [0, 1], [0, 2*pi], direction = 'negative'))
---
+++ laplace_equation.py
@@ -0,0 +1,138 @@
+import sympy as sym
+import re
+from sympy import cos, sin, pi, exp, cosh, sinh
+import partial_differentiation_functions as pdf
+x, y, z, i, j, k, t, u, v, r, a, b, c, n, L, w, T, s \
+    = sym.symbols('x y z i j k t u v r a b c n L w T s')
+
+# warning. not a reliable code. a_0 values incorrect but i ceebs to fucken fix it. should give the right summation
+# result. needs more testing though
+
+k = sym.symbols('k', real=True, positive=True)
+c = sym.symbols('c', real=True, nonzero=True)
+n = sym.symbols('n', integer=True)
+A, B, C, D = sym.symbols('A B C D')
+
+homogeneous = 'Y'
+if homogeneous == 'X':
+    X = A*cos(k*x) + B*sin(k*x)
+    Y = C*cosh(k*y) + D*sinh(k*y)
+else:
+    X = A*cosh(k*x) + B*sinh(k*x)
+    Y = C*cos(k*y) + D*sin(k*y)
+
+u_y = sym.diff(Y, y)
+u_x = sym.diff(X, x)
+
+period = a
+function = sin(x)
+
+# conditions = {X: {"term": 'X', "variable": None}, #  x, "values": {0: 0, a: 0}},
+#              Y: {"term": 'Y', "variable": None}, #  y, "values": {0: 0, a: function}},
+#              u_x: {"term": 'u_x', "variable": x, "values": {0: 0, a: function}},
+#              u_y: {"term": 'u_y', "variable": y, "values": {0: 0, pi: 0}}
+#              }
+
+conditions = {X: {"term": 'X', "variable": None},  # x, "values": {0: 0, a: 0}},
+              Y: {"term": 'Y', "variable": None},  # y, "values": {0: 0, a: function}},
+              u_x: {"term": 'u_x', "variable": x, "values": {0: 0, a: function}},
+              u_y: {"term": 'u_y', "variable": y, "values": {0: 0, pi: 0}}
+              }
+
+
+for keys, values in conditions.items():
+    if values['variable'] is not None:
+        for initial_value, results in values['values'].items():
+            try:
+                if sym.simplify(results).is_constant():
+                    variable = values['variable']
+                    boundary = keys.subs(variable, initial_value)
+                    solved_boundary = list(sym.solve(boundary, dict=True)[0].items())[0]
+                    string = str(solved_boundary[1])
+
+                    if re.search('tan', string):
+                        X = X.subs(k, n*pi/initial_value)
+                        u_x = u_x.subs(k, n*pi/initial_value)
+                        Y = Y.subs(k, n*pi/initial_value)
+                        u_y = u_y.subs(k, n*pi/initial_value)
+
+                    else:
+                        X = X.subs(solved_boundary[0], solved_boundary[1])
+                        u_x = u_x.subs(solved_boundary[0], solved_boundary[1])
+                        Y = Y.subs(solved_boundary[0], solved_boundary[1])
+                        u_y = u_y.subs(solved_boundary[0], solved_boundary[1])
+                else:
+                    function = results
+                    function_value = initial_value
+                    function_variable = values['variable']
+                    function_type = values['term']
+            except:
+                function_value = initial_value
+                function_variable = values['variable']
+                function_type = values['term']
+
+total_function = b*(Y*X).subs({A: 1, B: 1, C: 1, D: 1})
+print(total_function)
+
+if function_type == 'X' or function_type == 'Y':
+    diff = total_function.subs(function_variable, function_value)
+elif function_type == 'u_x':
+    diff = sym.diff(total_function, x).subs(function_variable, function_value)
+else:
+    diff = sym.diff(total_function, y).subs(function_variable, function_value)
+
+if homogeneous == 'X':
+    series = X
+elif homogeneous == 'Y':
+    series = Y
+
+if re.search('sin', str(series)):
+    bn = []
+    if type(function) is not list:
+        for i in [1, 2, 3]:
+            eq = (diff-function).subs(n, i)
+            solution = sym.solve(eq, dict=True)[0]
+            if not re.search('sin|cos|tan', str(solution[b])):
+                bn.append(solution[b])
+
+        if len(bn) == 1:
+            final = total_function.subs(b, bn[0])
+            print("Final solution to u(x,t): {}".format(final))
+        else:
+            b_n, b_1 = pdf.half_range_sine_series_continuous(function, period, x)
+            X = series.subs({A: 1, B: 1, C: 1, D: 1})
+
+            eq = (diff - b_n)
+            solution = sym.solve(eq, dict=True)[0]
+            print("u(x,t) is: summation of {}".format(solution[b] * X * total_function.subs(b, 1)))
+
+    else:
+        b_n, b_1 = pdf.half_range_sine_series_discontinuous(function[0], function[1], function[2], function[3])
+        X = series.subs({A: 1, B: 1, C: 1, D: 1})
+        eq = (diff - b_n)
+        solution = sym.solve(eq, dict=True)[0]
+        print("u(x,t) is: summation of {}".format(solution[b]*X*total_function.subs(b, 1)))
+
+
+elif re.search('cos', str(series)):
+    an = []
+    if type(function) is not list:
+        for i in [1, 2, 3]:
+            eq = (diff-function).subs(n, i)
+            solution = sym.solve(eq, dict=True)[0]
+            if not re.search('sin|cos|tan', str(solution[b])):
+                an.append(solution[b])
+
+        if len(an) == 1:
+            final = total_function.subs(b, an[0])
+            print("Final solution to u(x,t): {}".format(final))
+        else:
+            a_n, a_0 = pdf.half_range_cosine_series_continuous(function, period, x)
+            const = a_0 / 2
+            X = series.subs({A: 1, B: 1, C: 1, D: 1, b: 1})
+            eq = diff / X
+            print("Result is: {} + summation of {}".format(const, (a_n/eq*total_function).subs(b, 1)))
+
+    else:
+        a_n, a_0 = pdf.half_range_cosine_series_discontinuous(function[0], function[1], function[2], function[3])
+        print("\nu(x,t) is: {} + summation of {}".format(a_0/2, a_n*total_function.subs(b, 1)))
